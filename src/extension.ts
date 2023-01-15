5f3be11503d2646bc4a430a5ee10fc483a357405
Adam O'Brien <33055504+adamobrien4@users.noreply.github.com>
2023-01-15 10:50:03 +0000

    Added basic svelte webview.

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,28 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { KeybindTrainerPanel } from "./KeybindTrainerPanel";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("keybind-trainer.showWebview", () => {
+      KeybindTrainerPanel.createOrShow(context.extensionUri);
+    })
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "keybind-trainer" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('keybind-trainer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from keybind-trainer!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand("keybind-trainer.refresh", () => {
+      KeybindTrainerPanel.kill();
+      KeybindTrainerPanel.createOrShow(context.extensionUri);
+      setTimeout(() => {
+        vscode.commands.executeCommand(
+          "workbench.action.webview.openDeveloperTools"
+        );
+      }, 250);
+    })
+  );
 }
 
 // This method is called when your extension is deactivated
